c8cfdd77e2f84e262e4661a2ce96cad34a061c98
freshield <zxdsw199182@gmail.com>
2019-02-22 11:37:19 +0800

    done temperature part

---
+++ chp1/b2_temperature/cmake-build-debug/CMakeFiles/b2_temperature.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/b2_temperature.dir/main.c.o"
+  "b2_temperature.pdb"
+  "b2_temperature"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/b2_temperature.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chp1/cmake-build-debug/CMakeFiles/chp1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chp1.dir/main.c.o"
+  "chp1.pdb"
+  "chp1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chp1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
