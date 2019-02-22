0a0efb0a31bcb869efdf71fc5608dfac92147411
freshield <zxdsw199182@gmail.com>
2019-02-22 14:46:47 +0800

    done test"

--- chp1/b10_new_array/main.c
+++ chp1/b10_new_array/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int c, i, nwhite, nother;
+    int ndight[10];
+
+    nwhite = nother = 0;
+    for (int i = 0; i < 10; ++i) {
+        ndight[i] = 0;
+    }
+
+    while ((c = getchar()) != '!'){
+        if (c >= '0' && c <= '9'){
+            ++ndight[c-'0'];
+        }
+        else if (c == ' ' || c == '\t' || c == '\n'){
+            ++nwhite;
+        }
+        else {
+            ++nother;
+        }
+    }
+    printf("digits =");
+    for (int i = 0; i < 10; ++i) {
+        printf(" %d", ndight[i]);
+    }
+    printf(", white space = %d, other = %d\n", nwhite, nother);
 }
